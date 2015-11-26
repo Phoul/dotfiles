05438ffa4b689c83d56afd8411a9e779dc503f3d
Colin Childs <colin@torproject.org>
2015-11-26 16:35:16 -0600

    Bumping dotfiles

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -9,25 +9,36 @@ import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Layout.NoBorders
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.PerWorkspace (onWorkspace)
+import XMonad.Layout.Spiral
 import XMonad.Layout.Grid
 import XMonad.Layout.Named
+import XMonad.Util.Paste
 ---
+import XMonad.Actions.CopyWindow
+import qualified XMonad.StackSet as W
 
-myLayout = avoidStruts $ tiled ||| named "Mirror" (Mirror tiled) ||| Grid ||| noBorders Full 
+myLayout = avoidStruts $ tiled ||| named "Mirror" (Mirror tiled) ||| Grid ||| noBorders Full ||| spiral (6/7)  
  where
   tiled = named "Tall" (ResizableTall 3 (3/100) (1/2) [])
   boarderless = noBorders Full
 
 myFocusFollowsMouse :: Bool
 myFocusFollowsMouse = False
+--myFocusedBorderColor = "#990000"
+--myFocusedBorderColor = "#098cfe"
+myFocusedBorderColor = "#ccff2a"
 
 myManageHook = composeAll
     [ className =? "Gimp"      --> doFloat
-    , className =? "Iceweasel" --> doShift "F"
+    , className =? "Jitsi" --> doFloat
+    , className =? "Tor Browser" --> doShift "F"
     , className =? "Firefox" --> doShift "F"
-    , className =? "Evince" --> doShift "H"
+    , className =? "Firefox" --> doFloat
+    , className =? "Evince" --> doShift "I"
     , className =? "Icedove" --> doShift "I"
---    , className =? "Emacs" --> doShift "C" 
+    , className =? "Chromium" --> doShift "E"
+    , className =? "Pidgin" --> doFloat
+    , className =? "ricochet" --> doFloat
     ]
 
 main = do
@@ -42,9 +53,14 @@ main = do
                         , ppTitle = xmobarColor "green" "" . shorten 50
                         }
         , modMask = mod4Mask     -- Rebind Mod to the Super key
+	, focusedBorderColor = myFocusedBorderColor
 	, terminal = "urxvt"
 	, workspaces = ["A","B","C","D","E","F","G","H","I"]
         } `additionalKeys`
-        [ ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s -e 'mv $f ~/Pictures/Screenshots'")
+        [   
+	 ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s -e 'mv $f ~/Pictures/Screenshots'")
+	, ((mod4Mask, xK_p), spawn "dmenu_run -fn '-*-terminus-medium-*-*-22-*-*-*-*-*-*'")
+	, ((mod4Mask, xK_v ), windows copyToAll) -- @@ Make focused window always visible
+ 	, ((mod4Mask .|. shiftMask, xK_v ),  killAllOtherCopies) -- @@ Toggle window state back
 	]
 
